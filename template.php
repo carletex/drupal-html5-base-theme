76c13818f01dd59815c7a8ebe59d6f6ba357ebc2
Carlos Sánchez <oceanrdn@gmail.com>
2014-03-29 16:01:16 +0100

    Empty template.php file

--- template.php
+++ template.php
@@ -1,10 +1,2 @@
 <?php
-/**
- * Changes the default meta content-type tag to the shorter HTML5 version
- */
-/*function boron_html_head_alter(&$head_elements) {
-  $head_elements['system_meta_content_type']['#attributes'] = array(
-    'charset' => 'utf-8'
-  );
-}
-*/
+
